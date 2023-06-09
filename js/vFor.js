9e693c64c8ced73d3bb3b33b733e42b3604ffb11
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:43:57 -0500

    taller 14 Vue

---
+++ js/vFor.js
@@ -0,0 +1,13 @@
+console.log("Componente Vue");
+console.log(Vue);
+
+const app = Vue.createApp({
+ 
+});
+/* identificador el #
+se va configurar las opciones o mas conocido option api
+Option API es la estructura de mi pagina web (Opciones)
+-html
+-java script
+-css*/
+app.mount("#miAplicacion2");
